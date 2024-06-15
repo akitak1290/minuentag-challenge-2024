9d3e7df37d5185683f1556b18456c7b34e8dcdee
kien <akitak1290@gmail.com>
2024-06-15 15:51:19 -0400

    feat: add extra home component

--- ecommerce-app/src/home/index.js
+++ ecommerce-app/src/home/index.js
@@ -1,50 +1,103 @@
+import useSWR from 'swr';
+import { useNavigate } from 'react-router-dom';
+
 import styles from './styles.module.scss';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 /*
- * There are not requirement for this page,
+ * There are no requirements for this page,
  * so for the demo purpose some data will
  * be hardcoded for simplicity
 */
 export default function Home() {
+    const url = process.env.REACT_APP_SERVER_URL ?
+        process.env.REACT_APP_SERVER_URL : 'http://localhost:3001';
+    const productsEndpoint = `${url}/api/products`;
+
+    const { data, error, isLoading } = useSWR(productsEndpoint, fetcher);
+
+    if (isLoading) return <span className={styles.loading}>Loading</span>
+    if (error) return <span className={styles.loading}>Something when wrong</span>
+
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.nav}>
-                <button><img src="/icons/menu-icon.png" alt="menu"></img></button>
-                <span><img src="/icons/Sin ti╠ütulo-1.jpg" alt="avatar"></img></span>
+                <button className={styles.nav_burger}><img src="/icons/menu-icon.png" alt="menu"></img></button>
+                <span className={styles.nav_avatar}><img src="/icons/Sin ti╠ütulo-1.jpg" alt="avatar"></img></span>
             </div>
             <div className={styles.greeting}>
                 <p>Hi Mr. Michael,</p>
                 <p>Welcome Back!</p>
             </div>
-            <div className={styles.searchbar}>
-                <input placeholder='Search burger, pizza, drink, etc'></input>
+            <div className={styles.search_bar}>
+                <form>
+                    <input placeholder='Search burger, pizza, drink, etc'></input>
+                </form>
             </div>
             <div className={styles.category}>
                 <div>
                     <h3>Drink Category</h3>
-                    <p>See All</p>
+                    <p className={styles.expand_link}>See All</p>
                 </div>
                 <div>
                     <button>All</button>
-                    <button><img src="/icons/beer.png" alt="beer"/> Beer</button>
-                    <button><img src="/icons/wine-glass.png" alt="wine" />Wine</button>
+                    <button className={styles.last_pressed}>
+                        <span>
+                            <img src="/icons/beer.png" alt="beer" />
+                        </span>
+                        Beer
+                    </button>
+                    <button>
+                        <span>
+                            <img src="/icons/wine-glass.png" alt="wine" />
+                        </span>
+                        Wine
+                    </button>
                 </div>
             </div>
             <div className={styles.popular}>
                 <div>
                     <h3>Popular</h3>
-                    <p>See All</p>
+                    <p className={styles.expand_link}>See All</p>
                 </div>
-                <div>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div> 
+                <div className={styles.popular_cards}>
+                    {data.map((product, index) =>
+                        <DrinkCard id={product.id} brand={product.brand}
+                            image={product.image} price="2312" />
+                    )}
+                </div>
             </div>
         </div>
     );
 }
 
-const DrinkCard = ({name, image, price}) => {
-    return <></>
+const DrinkCard = ({ id, brand, image, price }) => {
+    const navigate = useNavigate();
+
+    // ! missing data for Lagunitas IPA, swap for Budweiser
+    // ! hardcoded for the demo
+    // if (brand === "Lagunitas IPA") {
+    //     brand = "Budweiser";
+    //     image = "/products/budweiser.jpg";
+    // }
+
+    const redirectToProduct = (id, brand) => {
+        navigate(`/${id}-${brand}`);
+    }
+
+    return (
+        <div onClick={() => redirectToProduct(id, brand)} className={styles.drink_card}>
+            <p>{brand}</p>
+            <div className={styles.drink_card_image_container}>
+                <img src={image} alt={brand} />
+            </div>
+            <span>
+                <p>$12.45</p>
+                <button onClick={(event) => event.stopPropagation()}>
+                    <img src='/icons/add-cart-icon.png' alt='+'/>
+                </button>
+            </span>
+        </div>
+    );
 }
