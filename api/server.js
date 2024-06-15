5c1b3e318dc96fd7689bc8385f22178d1cd329e5
kien <akitak1290@gmail.com>
2024-06-15 12:24:40 -0400

    feat: add cors config for express server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import product from './product/index.js';
+import cors from 'cors';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const corsOption = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOption));
 
 // both routes are related to data from a product
 // so they are put under product/ for now
